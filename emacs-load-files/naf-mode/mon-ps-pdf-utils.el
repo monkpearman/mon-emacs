da1d4385c88b4a07bc194389fc13f16b93df8113
S Pearman <monkpearman@icloud.com>
2024-04-12 11:09:04 -0400

    trivial formatting

--- emacs-load-files/naf-mode/mon-ps-pdf-utils.el
+++ emacs-load-files/naf-mode/mon-ps-pdf-utils.el
@@ -2,7 +2,7 @@
 ;; -*- mode: EMACS-LISP; -*-
 
 ;;; ================================================================
-;; Copyright © 2009-2010 MON KEY. All rights reserved.
+;; Copyright © 2009-2012 MON KEY. All rights reserved.
 ;;; ================================================================
 
 ;; FILENAME: mon-ps-pdf-utils.el
@@ -171,7 +171,7 @@
 ;; Foundation Web site at:
 ;; (URL `http://www.gnu.org/licenses/fdl-1.3.txt').
 ;;; ==============================
-;; Copyright © 2009-2010 MON KEY 
+;; Copyright © 2009-2012 MON KEY 
 ;;; ==============================
 
 ;;; CODE:

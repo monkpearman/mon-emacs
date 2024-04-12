5115dbef2e768c5cce0dcf60787294098883c712
S Pearman <monkpearman@icloud.com>
2024-04-12 11:11:15 -0400

    trivial formatting

--- emacs-load-files/naf-mode/mon-type-utils-vars.el
+++ emacs-load-files/naf-mode/mon-type-utils-vars.el
@@ -2,7 +2,7 @@
 ;; -*- mode: EMACS-LISP; -*-
 
 ;;; ================================================================
-;; Copyright © 2010-2011 MON KEY. All rights reserved.
+;; Copyright © 2010-2012 MON KEY. All rights reserved.
 ;;; ================================================================
 
 ;; FILENAME: mon-type-utils-vars.el
@@ -112,7 +112,7 @@
 ;; Foundation Web site at:
 ;; (URL `http://www.gnu.org/licenses/fdl-1.3.txt').
 ;;; ==============================
-;; Copyright © 2010-2011 MON KEY 
+;; Copyright © 2010-2012 MON KEY 
 ;;; ==============================
 
 ;;; CODE:
